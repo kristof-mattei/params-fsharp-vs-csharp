05da6526c6a3d31cff0480f3d0ca5432dbbca2a5
Kristof Mattei <github@kristofmattei.be>
2016-05-02 15:27:08 +0200

    Initial commit

---
+++ EntryPointCS/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EntryPointCS
+{
+    using Lib;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo1 = new Foo();
+            var foo2 = new Foo();
+
+            var asParams = Test.TestMethod(foo1, foo2);
+            var asArray = Test.TestMethod(new Foo[] { foo1, foo2 });
+        }
+    }
+}
---
+++ EntryPointFS/Program.fs
@@ -0,0 +1,12 @@
+﻿open Lib
+
+[<EntryPoint>]
+let main argv = 
+    
+    let foo1 = new Foo()
+    let foo2 = new Foo()
+
+    let asParams = Test.TestMethod(foo1, foo2)
+    let asArray = Test.TestMethod([| foo1; foo2 |])
+
+    0
